6c78c53bbd1bb5781e7995a4120dce905a64657a
Diego Asanza <f.asanza@gmail.com>
2023-05-06 12:29:59 +0200

    add simple backtrace information
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- test/support/crt/startup.c
+++ test/support/crt/startup.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "backtrace.h"
 
 #define WEAK __attribute__((weak))
 #define ALIAS(f) __attribute__((weak, alias(#f)))
@@ -131,6 +132,29 @@ void reset_handler(void)
 	exit(0);
 }
 
+void 
+decode_bfsr_ufsr(uint32_t bfsr, uint32_t ufsr)
+{
+    volatile uint8_t *bfar  = (volatile uint8_t *)0xE000ED38;
+
+    if (bfsr & (1 << 7)) {
+        printf("BFAR : 0x%08X\n", *bfar);
+    }
+    printf("UFSR DIVBYZERO    : %d\n", (ufsr & (1 << 9)) != 0);
+    printf("UFSR UNALIGNED    : %d\n", (ufsr & (1 << 8)) != 0);
+    printf("UFSR NOCP         : %d\n", (ufsr & (1 << 3)) != 0);
+    printf("UFSR INVPC        : %d\n", (ufsr & (1 << 2)) != 0);
+    printf("UFSR INVSTATE     : %d\n", (ufsr & (1 << 1)) != 0);
+    printf("UFSR UNDEFINSTR   : %d\n", (ufsr & (1 << 0)) != 0);
+    printf("BFSR BFARVALID    : %d\n", (bfsr & (1 << 7)) != 0);
+    printf("BFSR LSPERR       : %d\n", (bfsr & (1 << 5)) != 0);
+    printf("BFSR STKERR       : %d\n", (bfsr & (1 << 4)) != 0);
+    printf("BFSR UNSTKERR     : %d\n", (bfsr & (1 << 3)) != 0);
+    printf("BFSR IMPRECISERR  : %d\n", (bfsr & (1 << 2)) != 0);
+    printf("BFSR PRECISERR    : %d\n", (bfsr & (1 << 1)) != 0);
+    printf("BFSR IBUSERR      : %d\n", (bfsr & (1 << 0)) != 0);
+}
+
 void
 fault_handler_c(unsigned int *stack)
 {
@@ -139,39 +163,20 @@ fault_handler_c(unsigned int *stack)
     volatile uint16_t *ufsr  = (volatile uint16_t *)0xE000ED2A;
     volatile uint8_t *bfsr  = (volatile uint8_t *)0xE000ED29;
     volatile uint8_t *mmfsr = (volatile uint8_t *)0xE000ED28;
-    volatile uint8_t *bfar  = (volatile uint8_t *)0xE000ED38;
 
-    printf("================================================\n\n");
-    printf("PANIC!!! Fault Handler Called...\n");
-    printf("\n");
-    printf("Stacktrace: \n");
+    printf("\n======== PANIC! Fault Handler Called ===========\n\n");
+    printf("PC: 0x%08X - %s\n", stack[6], backtrace_function_name(stack[6]));
+    printf("LR: 0x%08X - %s\n", stack[5], backtrace_function_name(stack[5]));
+    printf("\nRegisters: \n");
     printf("R0   : 0x%08X\n", stack[0]);
     printf("R1   : 0x%08X\n", stack[1]);
     printf("R2   : 0x%08X\n", stack[2]);
     printf("R3   : 0x%08X\n", stack[3]);
     printf("R12  : 0x%08X\n", stack[4]);
-    printf("LR   : 0x%08X\n", stack[5]);
-    printf("PC   : 0x%08X\n", stack[6]);
     printf("XPSR : 0x%08X\n", stack[7]);
     printf("CFSR : 0x%08X\n", *cfsr);
-    if (*bfsr & (1 << 7)) {
-        printf("BFAR : 0x%08X\n", *bfar);
-    }
     printf("\n");
-    printf("UFSR DIVBYZERO    : %d\n", (*ufsr & (1 << 9)) != 0);
-    printf("UFSR UNALIGNED    : %d\n", (*ufsr & (1 << 8)) != 0);
-    printf("UFSR NOCP         : %d\n", (*ufsr & (1 << 3)) != 0);
-    printf("UFSR INVPC        : %d\n", (*ufsr & (1 << 2)) != 0);
-    printf("UFSR INVSTATE     : %d\n", (*ufsr & (1 << 1)) != 0);
-    printf("UFSR UNDEFINSTR   : %d\n", (*ufsr & (1 << 0)) != 0);
-    printf("BFSR BFARVALID    : %d\n", (*bfsr & (1 << 7)) != 0);
-    printf("BFSR LSPERR       : %d\n", (*bfsr & (1 << 5)) != 0);
-    printf("BFSR STKERR       : %d\n", (*bfsr & (1 << 4)) != 0);
-    printf("BFSR UNSTKERR     : %d\n", (*bfsr & (1 << 3)) != 0);
-    printf("BFSR IMPRECISERR  : %d\n", (*bfsr & (1 << 2)) != 0);
-    printf("BFSR PRECISERR    : %d\n", (*bfsr & (1 << 1)) != 0);
-    printf("BFSR IBUSERR      : %d\n", (*bfsr & (1 << 0)) != 0);
+    decode_bfsr_ufsr(*bfsr, *ufsr);
     printf("================================================\n");
-    sprintf(buf, "Hard Fault Called at PC Address 0x%08x!", stack[6]);
     exit(-1);
 }
