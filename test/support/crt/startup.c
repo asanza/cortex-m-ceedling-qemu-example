b05fc30238d84f27aefb84c3ced5f2dbd3f9dcc0
Diego Asanza <f.asanza@gmail.com>
2023-05-14 08:58:57 +0200

    add gcov dump
    Signed-off-by: Diego Asanza <f.asanza@gmail.com>

--- test/support/crt/startup.c
+++ test/support/crt/startup.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unity.h>
+#include <gcov.h>
 
 #define WEAK     __attribute__((weak))
 #define ALIAS(f) __attribute__((weak, alias(#f)))
@@ -68,6 +69,9 @@ void
 reset_handler(void);
 void
 main(void);
+
+static void
+dump_gcov_info (void);
 
 __attribute__((
     used, section(".isr_vector"))) void (*const g_interrupt_vector[])(void) = {
@@ -305,4 +309,66 @@ Default_Handler(void)
     sprintf(buf, "Default Handler called for vector: %d (%s)", vector, vector_name(vector));
     UNITY_TEST_FAIL(0, buf);
     exit(0);
+}
+
+/* The start and end symbols are provided by the linker script.  We use the
+   array notation to avoid issues with a potential small-data area.  */
+
+extern const struct gcov_info *const __gcov_info_start[];
+extern const struct gcov_info *const __gcov_info_end[];
+
+/* This function shall produce a reliable in order byte stream to transfer the
+   gcov information from the target to the host system.  */
+
+static void
+dump (const void *d, unsigned n, void *arg)
+{
+  (void)arg;
+  const unsigned char *c = d;
+  unsigned char buf[2];
+
+  for (unsigned i = 0; i < n; ++i)
+    fwrite (encode (c[i], buf), sizeof (buf), 1, stderr);
+}
+
+/* The filename is serialized to a gcfn data stream by the
+   __gcov_filename_to_gcfn() function.  The gcfn data is used by the
+   "merge-stream" subcommand of the "gcov-tool" to figure out the filename
+   associated with the gcov information. */
+
+static void
+filename (const char *f, void *arg)
+{
+  __gcov_filename_to_gcfn (f, dump, arg);
+}
+
+/* The __gcov_info_to_gcda() function may have to allocate memory under
+   certain conditions.  Simply try it out if it is needed for your application
+   or not.  */
+
+static void *
+allocate (unsigned length, void *arg)
+{
+  (void)arg;
+  return malloc (length);
+}
+
+/* Dump the gcov information of all translation units.  */
+
+static void
+dump_gcov_info (void)
+{
+  const struct gcov_info *const *info = __gcov_info_start;
+  const struct gcov_info *const *end = __gcov_info_end;
+
+  /* Obfuscate variable to prevent compiler optimizations.  */
+  __asm__ ("" : "+r" (info));
+
+  while (info != end)
+  {
+    void *arg = NULL;
+    __gcov_info_to_gcda (*info, filename, dump, allocate, arg);
+    fputc ('\n', stderr);
+    ++info;
+  }
 }
